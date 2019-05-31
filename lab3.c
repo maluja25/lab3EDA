b44382a728a0bc4b0b75b7128c7b3187dd23e7d1
malu <diego.bustamante.n@usach.cl>
2019-05-30 23:04:30 -0400

    hola

--- lab3.c
+++ lab3.c
@@ -99,7 +99,7 @@ void visualizarGrafo(Lista *lista){
             ar=aux->adyacencia;
             while(ar!=NULL){ 
                 printf(" %i",ar->vrt->dato);
-                //printf(" %i",ar->peso);
+                printf("(%i)",ar->peso);
                 ar=ar->siguiente;
             }
         }
@@ -132,28 +132,30 @@ void regular(Lista *lista ,int vertices){
 	}
 }
 */
-/*
+
 void GRADOMAX(int gradoMax,Lista *lista,int vertices){
+	printf("vertices %i\n",vertices);
 	srand (time(NULL));
 	int Azar;
-	int NumerosAzar[gradoMax+1];
+	int NumerosAzar[gradoMax+2];
 	int j = 0,fstop;
 	for(j = 0;j < gradoMax + 1;j++){
 		do{
-			Azar = rand()%(vertices);
+			Azar = rand()%(vertices)+1;
+			//printf("Azar %i\n",Azar);
 			fstop = buscarNumero(Azar,NumerosAzar,gradoMax);
 		}while(fstop);
 		NumerosAzar[j] = Azar;
-		//printf("el numero es :%i\n",NumerosAzar[j]);
+		printf("el numero es :%i\n",NumerosAzar[j]);
 	}
+	printf("hola\n");
 	int i=1;
 	while(i < gradoMax + 1){
-		insertarArista(lista,NumerosAzar[0],NumerosAzar[i]);
-		insertarArista(lista,NumerosAzar[i],NumerosAzar[0]);
+		insertarArista(lista,NumerosAzar[0],NumerosAzar[i],0);
+		insertarArista(lista,NumerosAzar[i],NumerosAzar[0],0);
 		i++;
 	}
 }
-*/
 void Vertice(int vertices,Lista *lista){
 	int i;
 	for(i=1;i<vertices+1;i++){
@@ -168,11 +170,21 @@ int CrearGrafo(char argv[],int argc){
 int n,m;
 int ciclo = 0;
 int gradoGrafo = 0;
+int totalpesos = 0;
 int contarAristas(Lista *lista){
   int **matriz = (int **)malloc(sizeof(int *)*lista->tamano+1);
   for (int i = 0; i < lista->tamano+1; ++i)
   {
     matriz[i] = (int *)malloc(sizeof(int ));
+  }
+  int i=2 ,j = 1;
+  for (int i = 1; i < lista->tamano+1; ++i)
+  {
+  	j = 0;
+  	for (int j = 1; j < lista->tamano+1; ++j)
+  	{
+  		matriz[i][j] = 0;
+  	}
   }
   Nodo *aux=lista->inicio;
   Arista  *ar;
@@ -184,8 +196,12 @@ int contarAristas(Lista *lista){
               if(matriz[aux->dato][ar->vrt->dato] == 0 || matriz[ar->vrt->dato][aux->dato] == 0){
                 matriz[aux->dato][ar->vrt->dato] = 1;
                 matriz[ar->vrt->dato][aux->dato] = 1;
-                if(aux->dato != ar->vrt->dato)
+                totalpesos = totalpesos + ar->peso;
+                printf("totalpesos %i\n",totalpesos);
+                if(aux->dato != ar->vrt->dato){
                   contador++;
+                  printf("contador %i\n",contador);
+                }
                 else{
                   ciclo = 1;
                 }
@@ -195,8 +211,18 @@ int contarAristas(Lista *lista){
       }
       aux=aux->siguiente;
   }
+  for (int i = 1; i < lista->tamano+1; ++i)
+  {
+  	j = 0;
+  	for (int j = 1; j < lista->tamano+1; ++j)
+  	{
+  		
+  		printf(" %i",matriz[i][j]);
+  	}
+  	printf("\n");
+  }
   int count1=0,count2=0;
-  int i=2 ,j = 1;
+  i=2 ,j = 1;
   while(j <=lista->tamano){
       if(matriz[1][j] == 1){
         count1++;
@@ -249,109 +275,242 @@ void ObtenerGradoMaxYMin(Lista *lista){
   printf("el grado minimo es : %i\n",Gmin);
 
 }
-void Pesos(char const *argv[]){
-  FILE * fp;
-  int nMax = 0;
-  fp=fopen(argv[2],"r");
-  char linea[MAX_LIN], *p;
-   // val es la variable que almacenara cada dato de la linea en dato entero para posteriormente guardarla en la matriz
-   int val;
-   // aqui reservamos memoria para la matriz con la funcion calloc
-   int **matriz= (int **) calloc(1000, sizeof(int*)) ;
-   //inicializamos m con valor -1
-   int pesos[100];
-   char aux;
-   int i = 0;
-   int x;
-   while(aux != EOF){
-    char *numero;
-    aux = fgetc(fp);
-    if(aux == ' '){
-      aux = fgetc(fp);
-      numero = &aux;
-      printf("numero %i",atoi(numero));
-      x = atoi(numero);
-    }
-    if(aux == '\n'){
-      printf("\n");
-    }
-    if(aux == ','){
-      aux = fgetc(fp);
-      numero = &aux;
-      //printf("numero %i\n",atoi(numero));
-      x = atoi(numero);
-      pesos[i] = x;
-      i++;
-    }
-   }
+void ArchivoSalida2(int vertices,int aritas,int ciclo,int gradoGrafo,int gradoMax,int gradoMin,int sumaPesos){
+	FILE *archivo;
+	archivo = fopen("parametros.out","w");
+	fprintf(archivo,"%i",vertices);
+	fprintf(archivo,"\n");
+	fprintf(archivo,"%i",aritas);
+	fprintf(archivo,"\n");
+	fprintf(archivo,"%i",ciclo);
+	fprintf(archivo,"\n");
+	fprintf(archivo,"%i",gradoGrafo);
+	fprintf(archivo,"\n");
+	fprintf(archivo,"%i",gradoMax);
+	fprintf(archivo,"\n");
+	fprintf(archivo,"%i",gradoMin);
+	fprintf(archivo,"\n");
+	fprintf(archivo,"%i",sumaPesos);
+	fprintf(archivo,"\n");
 }
 void ObtenerDatosGrafo(Lista *lista){
   int cantidadVertices,cantidadAristas;
   cantidadVertices = lista->tamano;
-  printf("la cantidadVertices %i\n",cantidadVertices);
   cantidadAristas=contarAristas(lista);
-  printf("la cantidadAristas %i\n",cantidadAristas);
-  printf("ciclo %i\n",ciclo);
-  printf("gradoGrafo %i\n",gradoGrafo);
   ObtenerGradoMaxYMin(lista);
-  //SumaPesosGrafo(lista);
+  ArchivoSalida2(cantidadVertices,cantidadAristas,ciclo,gradoGrafo,Gmax,Gmin,totalpesos);
 }
 void AnalizarGrafo(char const *argv[]){
-	FILE * fp;
+	Lista *lista = (Lista *)malloc(sizeof(Lista));
+	inicializar(lista);
+  FILE * fp;
   int nMax = 0;
-	fp=fopen(argv[2],"r");
-	char linea[MAX_LIN], *p;
-   // val es la variable que almacenara cada dato de la linea en dato entero para posteriormente guardarla en la matriz
-   int val;
-   // aqui reservamos memoria para la matriz con la funcion calloc
-   int **matriz= (int **) calloc(1000, sizeof(int*)) ;
-   Lista *lista = (Lista *)malloc(sizeof(Lista));
-   //inicializamos m con valor -1
-   m=-1;
-   while (fgets(linea, MAX_LIN, fp)!=NULL){
-     m=m+1;
-     //aqui reservamos memoria para cada uno de los espacios de la matriz
-     //printf("La linea es : %s\n", linea);
-     matriz[m] = (int *) calloc(MAX_LIN, sizeof(int));
-     p = strtok(linea," ");
-     n=-1;
-     while(p != NULL) {
-       n=n+1;
-       //con sscanf le damos le asignamos a val el valor que contiene p para posteriormente asigarle val a matriz en la posicion m,n
-       sscanf(p,"%i",&val);
-       //printf("%d\n",val);
-       if(n > nMax){
-        nMax = n;
-       }
-       matriz[m][n] = val;
-       p=strtok(NULL," ");
-       if(n == 0){
-       	insertarNodo(lista,matriz[m][n]);
-       } 
-     }
-   }
-    Pesos(argv);
-   	int i = 1;
-   	while(i <=5){
-   		int j = 1;
-   		while(j <= nMax){
-   			//printf("%i ",matriz[i-1][j]);
-   			if(matriz[i-1][j] > 0){
-   				insertarArista(lista,matriz[i-1][0],matriz[i-1][j],0);
+  fp=fopen(argv[2],"r");
+   int pesos[100];
+   char aux1,aux2;
+   int i = 0;
+   int x;
+   while(aux1 != EOF ){
+   		char *numero;
+   		aux1 = fgetc(fp);
+   		numero = &aux1;
+      	//printf("numero %i",atoi(numero));
+      	x = atoi(numero);
+   		if(aux1== ' '){
+   			while(aux1 != '\n' && aux1 != EOF){
+   				aux1 = fgetc(fp);
    			}
-   			j++;
    		}
-   		printf("\n");
-   		i++;
-   	}
-    ObtenerDatosGrafo(lista);
+   		if(x == 0){
+   			printf(" ");
+   		}
+   		else{
+   			//printf("%i \n",x);
+   			insertarNodo(lista,x);
+   		}
+   }
+   fclose(fp);
+   fp=fopen(argv[2],"r");
+   printf("\n\n\n");
+   int x1 = 1,x2 = 0,x3;
+   while(aux2 != EOF){
+    char *numero;
+    aux2 = fgetc(fp);
+    if(aux2 == ','){
+      aux2 = fgetc(fp);
+      numero = &aux2;
+      x3 = atoi(numero);
+      //printf("peso %i",x3);
+      insertarArista(lista,x1,x2,x3);
+    }
+    else if(aux2 == ' '){
+      //printf("-");
+      aux2 = fgetc(fp);
+      numero = &aux2;
+      x2 = atoi(numero);
+      //printf("peso %i-",x3);
+      //printf("numero %i",x2);
+      
+    }
+    if(aux2 == '\n'){
+      x1++;
+      //printf("\n");
+    }
+   }
+   printf("\n\n\n\n\n\n\n");
+   visualizarGrafo(lista);
+   fclose(fp);
+   ObtenerDatosGrafo(lista);
     //visualizarGrafo(lista);
-    fclose(fp);
+    //fclose(fp);
+}
+void insertarPesos(Lista * lista,int ini,int fin,int peso){
+	Nodo *aux=lista->inicio;
+    Arista  *ar;
+    //printf("Nodos\n");
+    while(aux!=NULL){
+    	if(aux->dato == ini){
+    		if(aux->adyacencia!=NULL){
+            	ar=aux->adyacencia;
+            	while(ar!=NULL){ 
+            		if(ar->vrt->dato == fin){
+            			if(ar->peso == 0){
+            				ar->peso = peso;
+            			}
+            			else{
+            				ar->peso  = ar->peso ;
+            			}
+            		}
+                	ar=ar->siguiente;
+            	}
+       	 	}
+    	}   
+        aux=aux->siguiente;
+    }
+}
+void GrafoRegular(int grado,int sumaPesos,int arista){
+	int i = 1,j;
+	Lista *lista = (Lista *)malloc(sizeof(Lista));
+	inicializar(lista);
+	while(i <=grado ){
+		insertarNodo(lista,i);
+		i++;
+	}
+	i = 1,j = 1;
+	int div = sumaPesos / arista;
+	printf("el div es %i\n",div);
+	int k = 1;
+	while(i <= grado){
+		j = 1;
+		while(j <= grado){
+			if(i != j)
+			{
+				insertarArista(lista,i,j,0);
+			}
+			j++;
+		}
+		i++;
+	}
+	visualizarGrafo(lista);
+	i = 1,j = 1,k = 0;
+	while(i <= grado){
+		j = 1;
+		while(j <=grado-k){
+			insertarPesos(lista,i,j,div);
+			insertarPesos(lista,j,i,div);
+			j++;
+		}
+		k++;
+		i++;
+	}
+	printf("k es %i\n",k);
+	i = 2;
+	/*
+	while(i <=grado){
+		insertarPesos(lista,k,i,div+sumaPesos%arista);
+		insertarPesos(lista,i,k,div+sumaPesos%arista);
+		i++;
+	}
+	*/
+	int L = contarAristas(lista);
+	printf("totalpesos %i\n",totalpesos);
+	printf("el numero de aristas es %i\n",L);
+	visualizarGrafo(lista);
+}
+void CrearGrafo(char const *argv[],int argc){
+	printf("argc %i\n",argc);
+
+	if(argc != 9){
+		printf("falta o sobra un dato\n");
+		return;
+	}
+	int i = 0;
+	int vertices,aristas,ciclos,regular,gradoMax,gradoMin,sumaPesos;
+	while(i < argc){
+		printf(" %s\n",argv[i]);
+		switch(i){
+			case 2:
+				vertices = atoi(argv[i]);
+			break;
+			case 3:
+				aristas = atoi(argv[i]);
+			break;
+			case 4:
+				ciclos =  atoi(argv[i]);
+			break;
+			case 5:
+				regular =  atoi(argv[i]);
+			break;
+			case 6:
+				gradoMax =  atoi(argv[i]);
+			break;
+			case 7:
+				gradoMin =  atoi(argv[i]);
+			break;
+			case 8:
+				sumaPesos  =  atoi(argv[i]);
+				printf("suma pesos %i\n",sumaPesos);
+			break;
+		}
+		i++;
+	}
+	if(aristas < vertices-1){
+		printf("la cantidad de aristas es menor a la cantidad de vertices-1\n");
+		printf("no se puede crear el grafo\n");
+		return ;
+	}
+	printf("regular %i\n",regular);
+	if(regular > 1|| regular < 0){
+		printf("el numero del grafo regular es distinto de 0 o 1\n");
+		printf("no se puede realizar un grafo regular\n");
+		return ;
+	}
+	if(ciclos < 0 || ciclos > 1 ){
+		printf("el numero del grafo con ciclos es distinto de 0 o 1\n");
+		printf("no se puede realizar un grafo regular\n");
+		return ;
+	}
+	if(regular == 1){
+		if(gradoMax != gradoMin){
+			printf("el grado maximo y el grado minimo son distintos\n");
+			printf("no se puede realizar un grafo regular\n");
+			return ;
+		}
+		else{
+			GrafoRegular(gradoMax,sumaPesos,aristas);
+			return;
+		}
+	}
+	Lista *lista = (Lista *)malloc(sizeof(Lista));
+	inicializar(lista);
+	Vertice(vertices,lista);
+	GRADOMAX(vertices,lista,gradoMax);
+	visualizarGrafo(lista);
 }
 void Inicio(int funcionalidad,char const *argv[],int argc){
 	switch(funcionalidad){
 		case 1:
-			//CrearGrafo(argv);
+			CrearGrafo(argv,argc);
 			return ;
 		break;
 		case 2:
